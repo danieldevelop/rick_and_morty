068b0cdc0cd7cec9e75e2d182383e8d815d734d2
Daniel Gomez <dgomez.informatica@gmail.com>
2023-01-18 23:49:46 -0300

    refactor: nuevo boton para agregar personajes random

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,18 @@
 import SearchBar from '../SearchBar/SearchBar.jsx'
-import estilo from './Nav.module.css'
+import estilo from './Nav.module.css';
+import cloud_download from "../../cloud-download.svg";
 
 const Nav = ({ onSearch }) => {
     return (
         <nav className={estilo.head__content}>
             <h1 className={estilo.head__contentTitle}>Rick&Morty</h1>
-            <SearchBar onSearch={onSearch} />
+
+            <div className={estilo.head__group}>
+                <button className={estilo.head__contentButton}>
+                    <img src={cloud_download} alt="icon bootstrap"/><i>Personaje</i>
+                </button>
+                <SearchBar onSearch={onSearch} />
+            </div>
         </nav>
     );
 }
