de62ff4048d87f805bb21306758c3ce326ce3b7f
Daniel Gomez <dgomez.informatica@gmail.com>
2023-01-18 21:57:38 -0300

    refactor: limpiado de condigo para que sea mas legible

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,9 +4,9 @@ import estilo from "./Cards.module.css";
 const Cards = ({ characters, onClose }) => {
    return <div className={estilo.cards}>
       {
-            characters.map(({id, name, species, gender, image}) => { // destructuring
+         characters.map(({id, name, species, gender, image}) => { // destructuring
             return <Card 
-               key={id} // character.id 
+               key={id} // character.id
                name={name} // character.name
                species={species}
                gender={gender}
