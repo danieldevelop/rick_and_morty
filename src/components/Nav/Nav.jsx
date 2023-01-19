51584b99716198dffd996abf6a048eeb275abaf3
Daniel Gomez <dgomez.informatica@gmail.com>
2023-01-19 01:43:14 -0300

    refactor: boton de personajes random funcional

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,13 +2,15 @@ import SearchBar from '../SearchBar/SearchBar.jsx'
 import estilo from './Nav.module.css';
 import cloud_download from "../../cloud-download.svg";
 
-const Nav = ({ onSearch }) => {
+const Nav = ({ onSearch, handleRandomPersonaje }) => {
     return (
         <nav className={estilo.head__content}>
-            <h1 className={estilo.head__contentTitle}>Rick&Morty</h1>
+            <h1 className={estilo.head__contentTitle}>
+                    Rick&Morty
+            </h1>
 
             <div className={estilo.head__group}>
-                <button className={estilo.head__contentButton}>
+                <button className={estilo.head__contentButton} onClick={handleRandomPersonaje}>
                     <img src={cloud_download} alt="icon bootstrap"/><i>Personaje</i>
                 </button>
                 <SearchBar onSearch={onSearch} />
