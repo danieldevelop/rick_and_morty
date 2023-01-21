73dbc6c7387c6776135f46fc47c57d56394a4856
Daniel Gomez <dgomez.informatica@gmail.com>
2023-01-20 22:11:06 -0300

    refactor: enlaces a about y pagina principal

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,16 @@
 import SearchBar from '../SearchBar/SearchBar.jsx'
 import estilo from './Nav.module.css';
 import cloud_download from "../../cloud-download.svg";
+import { Link } from 'react-router-dom';
 
 const Nav = ({ onSearch, handleRandomPersonaje }) => {
     return (
         <nav className={estilo.head__content}>
-            <h1 className={estilo.head__contentTitle}>
-                    Rick&Morty
-            </h1>
+            <div className={estilo.head__nav}>
+                {/* Esta etiqueta h1 es el remplazo del Home */}
+                <Link to="/"><h1 className={estilo.head__contentTitle}>Rick&Morty</h1></Link>
+                <Link to="/about" className={estilo.head__contentAbout}>Sobre mí</Link>
+            </div>
 
             <div className={estilo.head__group}>
                 <button className={estilo.head__contentButton} onClick={handleRandomPersonaje}>
