d960ceeac07c978c1e6551aa1713124bf54575df
Daniel Gomez <dgomez.informatica@gmail.com>
2023-02-12 12:44:02 -0300

    general: add formulario login

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,56 @@
+import estilo from "./Form.module.css";
+import { useState } from "react";
+import validation from "./validation";
+
+const Form = ({ login }) => {
+	const [userData, setUserData] = useState({
+		username: '',
+		password: ''
+	});
+
+	const [errors, setErrors] = useState({
+		username: '',
+		password: ''
+	})
+
+	const handleInputChange = (event) => {
+		setUserData({
+			...userData,
+			[event.target.name]: event.target.value
+		});
+
+		setErrors(validation({
+			...userData,
+			[event.target.name]: event.target.value
+		}));
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		login(userData);
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className={estilo.form}>
+			<h2 className={estilo.form_title}><span>&nbsp;Rick&Morty</span></h2>
+
+			<div className={estilo.form__group}>
+				<label htmlFor="username">Username:</label>
+				<input type="text" name="username" id="username" value={userData.username} onChange={handleInputChange}/>
+				{errors.username && <p className={estilo.form_error}>{errors.username}</p>}
+			</div>
+
+			<div className={estilo.form__group}>
+				<label htmlFor="password" style={{"margin-top": ".8rem"}}>Password:</label>
+				<input type="password" name="password" id="password" value={userData.password} onChange={handleInputChange}/>
+				{errors.password && <p className={estilo.form_error}>{errors.password}</p>}
+			</div>
+
+			<button className={estilo.form__btn_ingresar}>Ingresar</button>
+		</form>
+		
+	)
+}
+
+
+export default Form;
---
+++ src/components/Form/validation.js
@@ -0,0 +1,28 @@
+const validation = (userData) => {
+	let errors = {};
+
+	if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(userData.username)) { // Verifica formato
+		errors.username = "El email es inválido";
+	}
+
+	if (!userData.username) { // Si esta vacio
+		errors.username = "Este campo no puede estar vacío";
+	}
+
+	if (userData.username.length > 35) {
+		errors.username = "El email no puede superar los 35 caracteres"		
+	}
+
+	if (!userData.password.match(/\d/)) { // Verificar si contiene algun numero
+		errors.password = "La contraseña debe contener al menos un número";
+	}
+
+	if (userData.password.length < 6 || userData.password.length > 10) {
+		errors.password = "La contraseña debe contener entre 6 y 10 caracteres";
+	}
+
+	return errors;
+}
+
+
+export default validation;
